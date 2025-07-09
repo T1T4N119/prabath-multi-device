9a7ca279279501bf0a55803aa916baed08528342
𝗖𝗛𝗔𝗠𝗜𝗬𝗔𝗛𝗛...! <skylifecommunity0@gmail.com>
2025-07-09 13:54:59 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,4 +1,4 @@
-const DATABASE = "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" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
+const DATABASE = "mongodb://mongo:cmXALSMapkylJtGkipsBteyodnLYdFtk@mainline.proxy.rlwy.net:14788" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
 const BOT_NUMBER = "+94758244851" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
 const SESSION_ID = "PRABATH-MD~zIEEFAZT#rGUvVVTbqwyV08UBP-A9SuvsloI2unuEBFMDhDvZWIs" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
